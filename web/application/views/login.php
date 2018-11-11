73bf038417b9f9c45d8daf83856968967902c812
share424 <33365399+share424@users.noreply.github.com>
2018-11-11 10:45:51 +0700

    adding web

---
+++ web/application/views/login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pilih Menu Login Anda!</title>
+	 <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	 <script src="assets/js/jquery.min.js"></script>
+	 <script src="assets/js/popper.min.js"> </script>
+	 <script src="assets/js/bootstrap.min.js"></script>
+	 <link rel="stylesheet" type="text/css" href="assets/css/style/theme.css">
+</head>
+
+<body>
+	<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+		  <a class="navbar-brand" href="#" >
+		    <img src="assets/img/sentanu.png" alt="logo" class="logo-circle">
+	  	</a>
+ 		 <ul class="navbar-nav">
+    		<li class="nav-item">
+     	 		<a class="nav-link" href="home.html">Home</a>
+    		</li>
+    		<li class="nav-item">
+      			<a class="nav-link" href="home_next.html">Register</a>
+    		</li>
+    		<li class="nav-item">
+     			 <a class="nav-link" href="#">Pesan</a>
+    		</li>
+  		</ul>
+	</nav>
+	<div class="modal-dialog text-center">
+		<div class="col-sm-8 main-section">
+			<div class="modal-content">
+				<div class="col-12 user-img">
+					<img src="assets/img/omg.png" class="logo-circle">
+					
+				</div>
+				<form class="col-12">
+					<div>
+						<a href="login/member"><button type="button" class="btn btn-red">Login Member</button></a>
+					</div>
+					<div>
+						<a href="login/manager"><button type="button" class="btn btn-green">Login Manager</button></a>
+					</div>
+					<div>
+						<a href="login/tukang_jahit"><button type="button" class="btn btn-blue">Login Tukang Jahit</button></a>
+					</div>
+				</form>
+
+				<div class="col-12 forgot">
+					<a href="register">Don't Have Account</a>
+					
+				</div>
+				
+			</div>
+			
+		</div>
+	</div>
+</body>
+</html>
---
+++ web/application/views/register.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/all.css">
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/popper.min.js"> </script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="assets/css/style/theme.css">
+</head>
+
+<body>
+	<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+		  <a class="navbar-brand" href="#" >
+		    <img src="assets/img/sentanu.png" alt="logo" class="logo-circle">
+	  	</a>
+ 		 <ul class="navbar-nav">
+    		<li class="nav-item">
+     	 		<a class="nav-link" href="home.html">Home</a>
+    		</li>
+    		<li class="nav-item">
+      			<a class="nav-link" href="home_next.html">Register</a>
+    		</li>
+    		<li class="nav-item">
+     			 <a class="nav-link" href="#">Pesan</a>
+    		</li>
+    		<li class="nav-item">
+     			 <a class="nav-link" href="#">Sample</a>
+    		</li>
+  		</ul>
+	</nav>
+	<div class="container">
+		<div class="row">
+			<div class="promo col-sm-4">
+				<img src="assets/img/promoup.png" class="img-promo">
+			</div>
+			<div class="promo col-sm-4">
+				<img src="assets/img/promo1.png" class="img-promo">
+			</div>
+			<div class="promo col-sm-4">
+				<img src="assets/img/promo3.png" class="img-promo">
+			</div>
+		</div>
+	<div class="modal-dialog text-center">
+		<div class="col-sm-8 main-section">
+			<div class="modal-content">
+				<div class="col-12 user-img">
+					<img src="assets/img/omg.png" class="logo-circle">
+					
+				</div>
+				<form class="col-12">
+					<div>
+						<a href="register/member"><button type="button" class="btn btn-red" href="">Register Member</button></a>
+					</div>
+					<div>
+						<a href="register/tukang_jahit"><button type="button" class="btn btn-red" href="">Register Tukang Jahit</button></a>
+					</div>
+				</form>
+				
+			</div>
+			
+		</div>
+	</div>
+</body>
+</html>
